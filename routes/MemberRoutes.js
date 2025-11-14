3269541bc27dfb7a6aa9aa5066c0f4a7b16bbed6
Stibbop <stevensulayao@gmail.com>
2025-11-14 17:13:08 +0800

    Change PUT to PATCH for updating member details in MemberRoutes

--- routes/MemberRoutes.js
+++ routes/MemberRoutes.js
@@ -7,7 +7,7 @@ router.post('/', MemberController.createMember); // Route for Creating a Member
 router.get('/', MemberController.getAllMembers); // Route for Getting All Member Details
 router.get('/user/:userId', MemberController.getMembersByUser); // Route for Getting Members of a User
 router.get('/:id', MemberController.getMember); // Route for Getting A Member Details
-router.put('/:id', MemberController.updateMember); // Route for Updating Member Details
+router.patch('/:id', MemberController.updateMember); // Route for Updating Member Details
 router.delete('/:id', MemberController.deleteMember); // Route for Deleting a Member 
 
 module.exports = router;
