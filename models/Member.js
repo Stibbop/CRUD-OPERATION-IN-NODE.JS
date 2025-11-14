7b8e2d0ee57525096ac114cb5dac771d47845034
Stibbop <stevensulayao@gmail.com>
2025-11-14 15:10:40 +0800

    Rename user field to userId in Member schema for consistency

--- models/Member.js
+++ models/Member.js
@@ -23,7 +23,7 @@ const memberSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    user: {
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
     }
