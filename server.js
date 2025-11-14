ea74b74b0e4b5bb4c4966ad1fdaa9f61e8a05ef3
Stibbop <stevensulayao@gmail.com>
2025-11-14 12:32:43 +0800

    Fix server.js by adding routes and middleware

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(express.json());
-
 const app = express();
+
+//Middleware
+app.use(express.json());
 app.use(cors());
 
+//Routes
+const memberRoutes = require('./routes/MemberRoutes');
+app.use('/api', memberRoutes);
+
+//MongoDB Connection
 mongoose.connect(process.env.MONGO_URI) 
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Not Connected to MongoDB', err));
 
+//Starting of Server
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
