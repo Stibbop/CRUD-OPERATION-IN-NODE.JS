74171ecfea0bab1bf9e4133d00d5039f7f65db88
Stibbop <stevensulayao@gmail.com>
2025-11-14 12:42:11 +0800

    Update member routes to use '/api/members' endpoint and adjust route definitions

--- routes/MemberRoutes.js
+++ routes/MemberRoutes.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 const MemberController = require('../controllers/MemberController');
 
-router.post('/members', MemberController.createMember); // Route for Creating a Member
-router.get('/members/:id', MemberController.getMember); // Route for Getting Member Details
-router.put('/members/:id', MemberController.updateMember); // Route for Updating Member Details
-router.delete('/members/:id', MemberController.deleteMember); // Route for Deleting a Member 
+router.post('/', MemberController.createMember); // Route for Creating a Member
+router.get('/:id', MemberController.getMember); // Route for Getting Member Details
+router.put('/:id', MemberController.updateMember); // Route for Updating Member Details
+router.delete('/:id', MemberController.deleteMember); // Route for Deleting a Member 
 
 module.exports = router;
